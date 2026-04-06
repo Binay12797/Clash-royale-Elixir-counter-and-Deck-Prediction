286f258cf01445fd6bf9726a4e9cc47f00a83eff
Binay Bista <binay.bista.009@gmail.com>
2026-04-06 17:43:52 +0545

    take screenshot at 10fps and update at the same window and preasents like a video

--- main.py
+++ main.py
@@ -2,15 +2,25 @@ import mss  # mss (Multiple ScreenShots) is an ultra-fast, cross-platform librar
 import cv2
 import numpy as np
 
+fps = 10
+update_interval = int(1000/fps) #thousand mili second (1 sec) / fps , delay must be in int
+    
+# pre-create window ONCE before the loop with a unique handle
+cv2.namedWindow("Screen Capture", cv2.WINDOW_NORMAL)
+    
 with mss.mss() as sct:
     monitor = sct.monitors[1]
-    screenshot = sct.grab(monitor)
-    img=np.array(screenshot) #this will convert the screenshot to numpy array
+    
+    while True:
+        screenshot = sct.grab(monitor)
+        img=np.array(screenshot) #this will convert the screenshot to numpy array
 
-    #convert image form BGRA Blue, Green, Red, and Alpha/opacity (mss format) to BRG (opencv format)
-    img_bgr = cv2.cvtColor(img,cv2.COLOR_BGRA2BGR)
-    #cv2.imshow("Screen Capture", img_bgr)
-    cv2.imshow("Screen Capture", img)
+         #convert image form BGRA Blue, Green, Red, and Alpha/opacity (mss format) to BRG (opencv format)
+        img_bgr = cv2.cvtColor(img,cv2.COLOR_BGRA2BGR)
+        cv2.imshow("Screen Capture",img_bgr)
 
-    cv2.waitKey(0) #wait till an random key is pressed
-    cv2.destroyAllWindows()
+        key = cv2.waitKey(update_interval) & 0xFF
+        if key == ord('q'): #program will wait for some time and if q is not pressed next loop will continue
+            break
+
+cv2.destroyAllWindows()
